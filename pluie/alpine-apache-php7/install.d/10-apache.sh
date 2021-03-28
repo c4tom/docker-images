bf5b92dc37e4e927db3f3053e7ebe60ad1d58c46
Candido Tominaga <candido1212@gmail.com>
2021-03-28 01:35:23 -0300

    php7-pecl-xdebug

--- pluie/alpine-apache-php7/install.d/10-apache.sh
+++ pluie/alpine-apache-php7/install.d/10-apache.sh
@@ -6,4 +6,4 @@ apk add apache2 apache2-proxy \
 php7-apache2 php7-mbstring php7-session php7-phar php7-zlib php7-zip php7-ctype \
 php7-mysqli php7-xml php7-pdo_mysql php7-opcache php7-pdo php7-json php7-curl \
 php7-gd php7-mcrypt php7-openssl php7-dom  \
-# php-pdo_odbc php-soap php-pgsql
+php-pdo_odbc php-soap php-pgsql php7-pecl-xdebug
---
+++ pluie/alpine-apache-php7/install.d/40-enable-xdebug.sh
@@ -0,0 +1,2 @@
+sed -i "s|;zend_extension|zend_extension|i" /etc/php7/conf.d/50_xdebug.ini
+sed -i "s|;xdebug|xdebug|i" /etc/php7/conf.d/50_xdebug.ini
